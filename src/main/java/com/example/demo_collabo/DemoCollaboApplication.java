9f81da8d3e3b0f4fa640b0f9a14a6f270c78340d
ExpandSource <expandsource@gmail.com>
2024-06-13 15:03:33 +0900

    Initial commit

---
+++ src/main/java/com/example/demo_collabo/DemoCollaboApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_collabo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoCollaboApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoCollaboApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo_collabo/DemoCollaboApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo_collabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoCollaboApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
